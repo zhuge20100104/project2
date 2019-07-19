26c71b633fbbfad3536071385bbe597c86136d0d
zhuge20100104 <Fredric2010@outlook.com>
2019-07-19 16:43:31 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -3,7 +3,9 @@ import traceback
 
 def func():
     try:
+        print('test3')
         print("Try start")
+        print('test4')
         a = 1/0
         return True
     except Exception as e:
